926333f1efbe7c2afdb6e4697c8ba5197cdf7f2a
pekokparah <38381212+pekokparah@users.noreply.github.com>
2018-04-14 23:18:49 +0700

    Update MusicBot.js

--- MusicBot.js
+++ MusicBot.js
@@ -2,6 +2,7 @@ const { Client, Util } = require('discord.js');
 const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
+const opusscript = require("opusscript");
 
 const client = new Client({ disableEveryone: true });
 
