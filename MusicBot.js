0d421bbc72497c203f167155302cff8c52b28bf9
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-04-15 09:00:28 +0700

    Update MusicBot.js

--- MusicBot.js
+++ MusicBot.js
@@ -59,13 +59,16 @@ client.on('message', async msg => { // eslint-disable-line
 				try {
 					var videos = await youtube.searchVideos(searchString, 10);
 					let index = 0;
-					msg.channel.send("```
-`__**Song selection:**__
+					msg.channel.send(`
+__**Song selection:**__
 
 ${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
 
-Please provide a value to select one of the search results ranging from 1-10.
-					```");
+recovers music above 1 to 10
+
+this music is still Beta test by Alfian Verter!
+
+`);
 					// eslint-disable-next-line max-depth
 					try {
 						var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
