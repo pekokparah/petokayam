02be45188059c06f7fa6ee4501c822f4ad3d68f4
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-21 08:52:18 +0700

    Update setprefix.js

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -1,19 +1,21 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 
-exports.run = async (music, message, args, color) => {
-	if (!message.member.hasPermission("MANAGE_GUILD")) return message.reply("Sorry you can't do that!").then(msg => msg.delete(3000));
-	if (!args[0]) return message.channel.send(`Please input new prefix`).then(msg => msg.delete(3000));
-	let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+module.exports.run = async (bot, message, args) => {
 
-	prefixes[message.guild.id] = {
-		prefixes: args[0]
-	};
+  if(!message.member.hasPermission("MANAGE_GUILD")) return
+  if(!args[0]) return message.channel.send("Please specify something!")
 
-	fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
-		if (err) console.log(err);
-	});
-  console.log(args[0])
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+
+  prefixes[message.guild.id] = {
+    prefixes: args[0]
+  };
+
+  fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+    if (err) console.log(err)
+  });
+
 }
 
 exports.help = {
