5d235a39d45f6b0ed1612978bcdcf3f97fb8eb36
PatrickPoppy <38290702+PatrickPoppy@users.noreply.github.com>
2018-06-28 03:41:48 +0700

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -48,7 +48,6 @@ const voiceChannel = message.member.voiceChannel;
           .setColor(color)
           .setFooter("Please provide a value to select one of the search results ranging from 1-10.")
           
-          message.react("💿")
           let msgtoDelete = await message.channel.send({embed: embed});
           // eslint-disable-next-line max-depth
           try {
