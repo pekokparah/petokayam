d494d51cf44c458298fb053be35e6711b3086983
alfianverter <38290702+alfianverter@users.noreply.github.com>
2018-07-19 22:32:13 +0700

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -22,7 +22,7 @@ exports.run = async(music, message, args, color,  queue) => {
     message.channel.send({embed: new Discord.RichEmbed()
                     .setAuthor(message.author.tag,message.author.avatarURL)
                     .setDescription(`The player has been stopped.`)
-                    .setColor('RANDOM')
+.setColor("#ec0000")
                    })
     return undefined; 
 
