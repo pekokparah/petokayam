a852199affb0b078d816890a3196ba22102d7a3d
alfianverter <38290702+alfianverter@users.noreply.github.com>
2018-07-20 17:05:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const queue = new Map();
 
 music.on('message', async message => {
 
-    let prefix = 'm.';
+    let prefix = '>';
     let msg = message.content.toLowerCase();
     let args = message.content.slice(prefix.length).trim().split(" ");
     let cmd = args.shift().toLowerCase();
